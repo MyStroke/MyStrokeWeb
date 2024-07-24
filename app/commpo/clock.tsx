dff59ad58a76d8923dc652e03409b2889291e321
TK17250 <TXK285@gmail.com>
2024-07-24 11:42:26 +0700

    Fix clock.

--- app/commpo/clock.tsx
+++ app/commpo/clock.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
@@ -8,11 +10,14 @@ const Clock = () => {
     return () => clearInterval(timerId); // Cleanup interval on component unmount
   }, []);
 
-  const formattedTime = time.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  const formattedTime = time.toLocaleTimeString('th-TH', { hour: 'numeric', minute: 'numeric' });
 
   return (
-    <div style={{ fontSize: '24px', fontWeight: 'bold' }}>
-      {formattedTime}
+    <div style={{ fontSize: '40px', fontWeight: 'bold' }} className='flex items-center'>
+      <div className="mr-3">
+        {formattedTime <= '18:59' ? <FontAwesomeIcon icon={faSun} color='orange' /> : <FontAwesomeIcon icon={faMoon} color='yellow' />}
+      </div>
+      {formattedTime} น.
     </div>
   );
 };
