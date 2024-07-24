b678f76e8f934df7ca190c8a15562414caa6d35f
TK17250 <TXK285@gmail.com>
2024-07-24 11:26:36 +0700

    Fix UI dashboard page.

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Page() {
         {
           label: 'New Patients',
           data: [50, 40, 60, 80, 100, 90, 70],
+          borderRadius: 10,
         },
       ],
     });
@@ -27,22 +28,23 @@ export default function Page() {
         {
           label: 'Old Patients',
           data: [45, 35, 55, 75, 95, 85, 65],
+          borderRadius: 10,
         },
       ],
     });
   }, []);
 
   return (
-    <div className="md:flex h-screen overflow-scroll">
+    <div className="md:flex h-screen overflow-scroll no-scrollbar">
       {/* Sidebar */}
       <div className="w-full md:w-80 lg:w-64 sidebar no-scrollbar">
         <Sidebar />
       </div>
 
       {/* Main Content */}
-      <div className="flex flex-col flex-grow p-8 pt-0 pb-0 text-white overflow-y-scroll no-scrollbar ">
+      <div className="flex flex-col flex-grow pt-0 pb-0 text-white overflow-y-scroll no-scrollbar ">
         {/* Search Bar */}
-        <div className="flex items-center fixed mt-8">
+        <div className="flex items-center fixed mt-8 ml-8">
           <i className="fa-solid fa-magnifying-glass text-2xl mr-4"></i>
           <input
             type="text"
@@ -52,40 +54,73 @@ export default function Page() {
         </div>
 
         {/* Main Content */}
-        <div className="flex mt-24 flex-col items-center justify-center h-full overflow-scroll">
+        <div className="flex mt-24 flex-col items-center justify-center overflow-scroll">
           {/* Large Square with Inner Squares */}
           <div className="bg-[#354151] rounded-lg p-8 w-full max-w-4xl lg:mt-64 xl:mt-20">
-            <div className="mb-8 text-2xl font-bold">Statistic</div>
-            <div className="flex justify-between mb-12">
-              <div className="bg-white text-black rounded-lg p-4 w-32 h-16 flex items-center justify-center">
-                <i className="fa-solid fa-user-group text-3xl"></i>
+            <div className="text-2xl font-bold">Statistic</div>
+            <div className="block lg:flex justify-between">
+              {/* Patients */}
+              <div className="bg-[#253345] text-black rounded-[12px] w-full px-4 py-2 m-5 mb-0 flex items-center">
+                <i className="fa-solid fa-user-group text-3xl text-[#7EA9FC] mr-3"></i>
+                <div>
+                  <p className="text-white text-xl font-bold">7</p>
+                  <p className='text-[#BBBBBB] text-sm'>ผู้ป่วย</p>
+                </div>
               </div>
-              <div className="bg-white text-black rounded-lg p-4 w-32 h-16 flex items-center justify-center">
-                <i className="fa-solid fa-clipboard-list text-3xl"></i>
+
+              {/* History */}
+              <div className="bg-[#253345] text-black rounded-[12px] w-full px-4 py-2 m-5 mb-0 flex items-center">
+                <i className="fa-solid fa-clipboard-list text-3xl text-[#7EA9FC] mr-3"></i>
+                <div>
+                  <p className="text-white text-xl font-bold">2</p>
+                  <p className='text-[#BBBBBB] text-sm'>ประวัติการบำบัด</p>
+                </div>
               </div>
-              <div className="bg-white text-black rounded-lg p-4 w-32 h-16 flex items-center justify-center">
-                <i className="fa-solid fa-users text-3xl"></i>
+              
+              {/* Classes */}
+              <div className="bg-[#253345] text-black rounded-[12px] w-full px-4 py-2 m-5 mb-0 flex items-center">
+                <i className="fa-solid fa-users text-3xl text-[#7EA9FC] mr-3"></i>
+                <div>
+                  <p className="text-white text-xl font-bold">1</p>
+                  <p className='text-[#BBBBBB] text-sm'>คลาสทั้งหมด</p>
+                </div>
               </div>
-              <div className="bg-white text-black rounded-lg p-4 w-32 h-16 flex items-center justify-center">
-                <i className="fa-regular fa-face-smile text-3xl text-[#61FF29]"></i>
+
+              {/* Feelings */}
+              <div className="bg-[#253345] text-black rounded-[12px] w-full px-4 py-2 m-5 mb-0 flex items-center">
+                <i className="fa-regular fa-face-smile text-3xl text-[#61FF29] mr-3"></i>
+                <div>
+                  <p className="text-white text-xl font-bold">2</p>
+                  <p className='text-[#BBBBBB] text-sm'>ความรู้สึกผู้ป่วย</p>
+                </div>
               </div>
+
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-8 mt-7">
-            <div className="bg-[#354151] text-white rounded-lg p-4 w-full h-52 items-center justify-center">
+
+          {/* Patients statics */}
+          <div className="grid grid-cols-2 gap-8 mt-7 w-full max-w-4xl">
+
+            {/* New */}
+            <div className="bg-[#354151] text-white rounded-lg p-4 w-full items-center justify-center">
               <div className='flex text-xl'>New patients statics
                 <i className=" ml-auto block fa-solid fa-chart-line text-xl text-[#26FF03]"></i>
               </div>
-              <div><ChartComponent data={newPatientsData} gradientFrom="#FFFFFF" gradientTo="#7B7FDF" /></div>
-
+              <div>
+                <ChartComponent data={newPatientsData} gradientFrom="#FFFFFF" gradientTo="#7B7FDF" />
+              </div>
             </div>
-            <div className="bg-[#354151] text-white rounded-lg p-4 w-full h-52  items-center justify-center">
+
+            {/* Old */}
+            <div className="bg-[#354151] text-white rounded-lg p-4 w-full  items-center justify-center">
               <div className='flex text-xl'><div className='text-[#FFF970]'>Old &nbsp; </div> patients statics
                 <i className=" ml-auto block fa-solid fa-chart-line text-xl text-[#FFF970]"></i>
               </div>
-              <ChartComponent data={oldPatientsData} gradientFrom="#FFFFFF" gradientTo="#E9CA5D" />
-
+              <div className="">
+                <ChartComponent data={oldPatientsData} gradientFrom="#FFFFFF" gradientTo="#E9CA5D" />
+              </div>
             </div>
+
           </div>
 
           {/* Upcoming Appointment */}
